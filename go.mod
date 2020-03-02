020a595dd47dedbbbaad0f5d72cb363594fc5ba8
ablades <audaris@gmail.com>
2020-03-02 12:14:46 -0800

    update env

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module rr
 
 go 1.13
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/joho/godotenv v1.3.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
--- main.go
+++ main.go
@@ -7,6 +7,37 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type redditAuth struct {
+	ClientID      string
+	ClientSecret  string
+	response_type "code"
+}
+
+// func future_auth() {
+// 	ctx := context.Background()
+
+// 	oauth2.SetAuthURLParam("key", "value")
+
+// 	conf := &oauth2.Config{
+// 		ClientID:     os.Getenv("CLIENT"),
+// 		ClientSecret: os.Getenv("REDDIT_SECRET"),
+// 		Scopes:       nil,
+// 		Endpoint:     oauth2.Endpoint{},
+// 	}
+
+// //Authentication configuration variable
+// var OauthConfig *oauth2.Config
+
+// //Load Environment Variables
+// err := godotenv.Load()
+// if err != nil {
+// 	log.Fatal("Error loading environment variables")
+// }
+
+// secretKey := os.Getenv("REDDIT_SECRET")
+
+// }
+
 func main() {
 
 	//Load Environment Variables
@@ -15,6 +46,9 @@ func main() {
 		log.Fatal("Error loading environment variables")
 	}
 
-	secretKey := os.Getenv("REDDIT_SECRET")
+	secretKey := os.Getenv("SCRIPT_REDDIT_SECRET")
+	client := os.Getenv("SCRIPT_CLIENT")
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
 
 }
