c599e55cd75a15bc8d037bcdec63a019374e343c
ablades <audaris@gmail.com>
2020-05-01 18:47:21 -0700

    add is terminal

--- tries/prefixnode.go
+++ tries/prefixnode.go
@@ -5,4 +5,18 @@ type prefixNode struct {
 	char     string
 	terminal bool
 	children map[string]*prefixNode
+	users []string
+}
+
+//New creates a node with character and terminal value
+func New(char string, terminal bool) prefixNode {
+
+	node := prefixNode{char, terminal, map[string]*prefixNode{}, []string}
+
+	return node
+}
+
+//
+func (node prefixNode) isTerminal() {
+	return node.terminal
 }
