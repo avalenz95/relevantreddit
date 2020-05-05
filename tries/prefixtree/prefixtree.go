630f244d9ecbf7729266e6585e96195ba388bc26
ablades <audaris@gmail.com>
2020-05-04 23:08:14 -0700

    add testing file and getters to prefix tree

--- tries/prefixtree/prefixtree.go
+++ tries/prefixtree/prefixtree.go
@@ -6,16 +6,17 @@ import (
 
 //PrefixTree for nodes
 type PrefixTree struct {
-	subredditName string
-	root          *prefixnode.Node
-	size          int
+	name string
+	root *prefixnode.Node
+	size int
 }
 
 //New tree with a default root node
-func New() PrefixTree {
+func New(name string) PrefixTree {
 	rootNode := prefixnode.New('0')
 
 	tree := PrefixTree{
+		name: name,
 		root: &rootNode,
 		size: 0,
 	}
@@ -55,8 +56,24 @@ func (tree PrefixTree) contains(word string) []string {
 	for _, char := range word {
 		if node.HasChild(char) {
 			node = node.GetChild(char)
+		} else {
+			return nil
 		}
 	}
 
 	return node.GetUsers()
+}
+
+//Getters and setters
+func (tree PrefixTree) getName() string {
+	return tree.name
+}
+
+func (tree PrefixTree) getSize() int {
+	return tree.size
+}
+
+//Testing purposes
+func (tree PrefixTree) getRoot() *prefixnode.Node {
+	return tree.root
 }
---
+++ tries/prefixtree/prefixtree_test.go
@@ -0,0 +1,10 @@
+package prefixtree_test
+
+import (
+	"rr/tries/prefixtree"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tree := prefixtree.New("Test")
+}
