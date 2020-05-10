c44c4e37df2445398c1ed83a2055cbbe25be33fa
ablades <audaris@gmail.com>
2020-05-10 12:25:01 -0700

    add nav logic

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 let endpoint = "http://localhost:8080"
 
-
 function Dashboard() {
 
-    
+
     return(
         <div className="dashboard">
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,10 +5,25 @@ import axios from 'axios'
 function Nav(props) {
 
     const {endpoint} = props
+    let isAuthenticated = false
+
+    //Authentication request
+    function onAuth () {
+        axios.get(endpoint + "/r/login").then((response) => {
+            console.log(response)
+            //Redirect user
+            window.location.replace(response.request.responseURL)
+            isAuthenticated = true
+        })
+    } 
 
     return (
         <nav className="navbar">
-
+            {isAuthenticated ? 
+            <span>Welcome!</span> 
+            : 
+            <button type="submit" onClick={onAuth}>Authenticate with Reddit</button>
+            }
         </nav>
     )
 }
