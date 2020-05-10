17b581a5ef26f989b8002c694f7e1308e0fc9c5c
ablades <audaris@gmail.com>
2020-05-10 12:13:35 -0700

    add nav and dashboard skeletons

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+let endpoint = "http://localhost:8080"
+
+
+function Dashboard() {
+
+    
+    return(
+        <div className="dashboard">
+
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+
+//Navigation for website
+function Nav(props) {
+
+    const {endpoint} = props
+
+    return (
+        <nav className="navbar">
+
+        </nav>
+    )
+}
+
+export default Nav
