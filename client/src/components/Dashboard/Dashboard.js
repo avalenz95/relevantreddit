fe70ec9d8db69fcb56e2edbc9a310fe42042895f
ablades <audaris@gmail.com>
2020-05-10 19:44:23 -0700

    update dashboard grid and card

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import axios from 'axios'
 import Cookies from 'js-cookie'
+import Grid from '../Grid/Grid.js'
 
 let endpoint = "http://localhost:8080"
 
 function Dashboard() {
     //Gets the users reddit content from db
     let userName = Cookies.get("username")
+    let subreddits = null
 
-    //figure out a way to make sure this doesnt happen every single time the dashboard loads
-    function getData() {
-        if (userName) {
-            axios.get(endpoint + "/user/" + userName).then(response => {
-                console.log(response)
-                console.log("endpoint reached")
-                //response.data.username
-                //response.data.subreddits
-                //getImages
-            })
-        }
-
+//figure out a way to make sure this doesnt happen every single time the dashboard loads
+    if (userName) {
+        axios.get(endpoint + "/user/" + userName).then(response => {
+            console.log(response)
+            console.log("endpoint reached")
+            subreddits = response.data.subreddits
+        })
     }
 
     return(
         <div className="dashboard">
-            
+            <Grid
+                endpoint={endpoint}
+                userName={userName}
+                subreddits={subreddits}
+
+            />
         </div>
     )
 }
