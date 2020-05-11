9c0f0613430c027e05b7b570ef0fd63024946f3c
ablades <audaris@gmail.com>
2020-05-11 12:18:15 -0700

    add keyword form

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,34 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Card.css'
 
 function Card(props) {
     const {subName, imgUrl, keywords} = props
+
+    const [word, setWord] = useState("")
+
+
     let style = {
-        card: {
-            backgroundImage: `url(${imgUrl})`,
+        container: {
         }
     }
 
+    //List of keyword buttons
     let tags = []
-
+    //Add button for each keyword
     for (var i = 0; i < keywords.length; i++) {
         tags.push(
             <button>{keywords[i]}</button>
         )
     }
 
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
         //Card content
-        <div className="card" style={style.card}>
-            <div className="container">
+        <div className="card">
+            <div className="container" style={style.container}>
                 
                 <h4><b>{subName}</b></h4>
 
                 <div>
                     {tags}
                 </div>
-
-                <button type="input">Add Keyword</button>
+                <form onSubmit={handleSubmit}>
+                    <label> Add New Keyword: 
+                        <input type="text" value={word} onChange={event => setWord(event.target.value)}/>
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         </div>
         
