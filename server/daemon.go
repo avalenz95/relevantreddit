3e99ec8ebe35c02eb68e0162356eb39c384a6e88
ablades <audaris@gmail.com>
2020-07-30 10:49:24 -0500

    no blocks

--- server/daemon.go
+++ server/daemon.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -15,8 +14,8 @@ import (
 )
 
 //pass in subreddit includes /r get posts from x time period
-func fetchSubredditPosts(trie *SubTrie, queue chan notifcation, wg *sync.WaitGroup) {
-	//defer wg.Done() // wait for goroutine to finish before decrementing
+func fetchSubredditPosts(trie *SubTrie, queue chan map[string][]string, wg *sync.WaitGroup) {
+	defer wg.Done() // wait for goroutine to finish before decrementing
 	defer fmt.Printf("\033[32m Done with %s \033[0m", trie.Subname)
 	//build the initial url
 	url := fmt.Sprintf("https://api.reddit.com/%s/new", trie.Subname) // best temporarily for consistent input data
@@ -49,7 +48,7 @@ func fetchSubredditPosts(trie *SubTrie, queue chan notifcation, wg *sync.WaitGro
 }
 
 //parse comments for a given subreddit post
-func fetchComments(relPath string, trie *SubTrie, queue chan notifcation) {
+func fetchComments(relPath string, trie *SubTrie, queue chan map[string][]string) {
 
 	//Url to comments of a post
 	url := fmt.Sprintf("https://api.reddit.com%s", relPath)
@@ -68,34 +67,30 @@ func fetchComments(relPath string, trie *SubTrie, queue chan notifcation) {
 	var comments redditComments
 
 	json.Unmarshal(data, &comments)
+
+	noteMap := make(map[string][]string)
 	//Process Comment
 	for _, c := range comments {
 		for _, comment := range c.Data.Children {
 			//Check words against Trie
-			processComment(comment.Data.Body, trie, queue)
-		}
-	}
-}
+			r := strings.NewReplacer(",", "", ".", "", ";", "")
+			parsedComment := strings.Fields(r.Replace(comment.Data.Body))
 
-//Strip comment of punctuation and other characters
-func processComment(comment string, trie *SubTrie, queue chan notifcation) {
-	fmt.Printf("  ---------  Processing comment: %s \n", comment)
-	//Remove links and urls
-	re, _ := regexp.Compile(`((\[(.+)\]\(.+\))|(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*))`)
-
-	r := strings.NewReplacer(",", "", ".", "", ";", "")
-	parsedComment := strings.Fields(r.Replace(comment))
-
-	for _, word := range parsedComment {
-		users := trie.Tree.Contains(word)
-		if len(users) > 0 {
-			for _, user := range users {
-				fmt.Printf("\033[32m Added Notification to channel for User: %s  with word: %s \033[0m \n ", user, word)
-				//Add to channel
-
+			for _, word := range parsedComment {
+				users := trie.Tree.Contains(word)
+				if len(users) > 0 {
+					for _, user := range users {
+						fmt.Printf("\033[32m Added Notification to channel for User: %s  with word: %s \033[0m \n ", user, word)
+						//Add to channel
+						msg := fmt.Sprintf("Comment contains %s: \n %s \n", word, comment.Data.Body)
+						noteMap[user] = append(noteMap[user], msg)
+					}
+				}
 			}
 		}
 	}
+
+	queue <- noteMap
 
 }
 
@@ -105,14 +100,10 @@ func parsePosts(posts []redditPosts) {
 }
 
 //
-type notifcation struct {
-	name string
-	msg  string
-}
 
 func daemon() {
 	//Make a notification map
-	//notificationMap := make(map[string][]string)
+	//noteMap := make(map[string][]string)
 	//Anytime a keyword returns add that post to users notification map
 	// Get Tries Collection
 	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Second)
@@ -132,26 +123,29 @@ func daemon() {
 
 	//Maintains count of go routines
 	var wg sync.WaitGroup
-
-	notificationQueue := make(chan notifcation)
-	fmt.Println("Tries")
-	//Gets posts for each trie concurrently
-	wgCounter := 0
-	for _, trie := range allTries {
-		fmt.Printf(" %s \n", trie.Subname)
-		wgCounter++
-		//wg.Add(1)
-		fetchSubredditPosts(trie, notificationQueue, &wg)
-		fmt.Println(wgCounter)
-	}
-	fmt.Println("BEFORE WAIT")
-	//wg.Wait() // Wait till goroutines finish
-	fmt.Println("AFTER WAIT")
-	close(notificationQueue) // close channel - no more values will be added
+	masterMap := make(map[string][]string)
+	noteQueue := make(chan map[string][]string, len(allTries))
 
 	//Create Map based off values in channel
-	notificationMap := make(map[string][]string)
+	fmt.Println("Tries")
+	//Gets posts for each trie concurrently
+	for _, trie := range allTries {
+		fmt.Printf(" %s \n", trie.Subname)
+		wg.Add(1)
+		go fetchSubredditPosts(trie, noteQueue, &wg)
+	}
 
-	fmt.Printf("\n --Map of Notifications-- \n  %+v \n  END DAEMON :))))) \n \n", notificationMap)
+	//Read from channel until it's closed
+	go func() {
+		for note := range noteQueue {
+			fmt.Println(note)
+		}
+	}()
+
+	wg.Wait() //Wait till all goroutines are finished before continuing
+	fmt.Println("AFTER WAIT")
+	close(noteQueue) // close channel - no more values will be added
+
+	fmt.Printf("\n --Map of Notifications-- \n  %+v \n  END DAEMON :))))) \n \n", masterMap)
 
 }
